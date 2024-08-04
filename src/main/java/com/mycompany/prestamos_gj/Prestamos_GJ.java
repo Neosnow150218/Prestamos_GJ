2a38135b90f6a486eced87ac2893b25dcbfba1fd
J.C <shadowassassin1607@gmail.com>
2024-08-04 16:56:42 -0400

    Main

---
+++ src/main/java/com/mycompany/prestamos_gj/Prestamos_GJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prestamos_gj;
+
+/**
+ *
+ * @author juan1
+ */
+public class Prestamos_GJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
